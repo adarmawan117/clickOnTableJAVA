aa4ed4ce08e33af326d1c178ece6e43d5fc7d81a
Agus Darmawan <46953807+adarmawan117@users.noreply.github.com>
2022-01-11 17:23:47 +0700

    Menambah cara set JDateChooser dari tabel

--- temp.java
+++ temp.java
@@ -9,5 +9,26 @@
             dateTanggalExp.setDateFormatString(tabelObat.getValueAt(i, 4).toString());
             comboBoxJenis.setSelectedItem(tabelObat.getValueAt(i, 5).toString());
             txtStok.setText(tabelObat.getValueAt(i, 6).toString());
+	    /*
+	    
+	    CARA LAIN : https://stackoverflow.com/a/47927726
+	    
+	    Calendar ca = new GregorianCalendar();
+	String day = ca.get(Calendar.DAY_OF_MONTH) + "";
+	String month = ca.get(Calendar.MONTH) + 1 + "";
+	String year = ca.get(Calendar.YEAR) + "";
+
+	if (day.length() == 1) {
+	    day = "0" + day;
+	}
+	if (month.length() == 1) {
+	    month = "0" + month;
+	}
+
+	String dd = year + "-" + month + "-" + day;
+
+	Date date = new SimpleDateFormat("yyyy-MM-dd").parse(dd);
+	jDateChooser1.setDate(date);
+	    */
             dateTanggalMasuk.setDateFormatString(tabelObat.getValueAt(i, 7).toString());
         }
